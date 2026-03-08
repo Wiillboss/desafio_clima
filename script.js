03518327b4fe37ff57df3a3ae0599aaffe49564a
Willian dos Santos Silva <86166759+Wiillboss@users.noreply.github.com>
2026-03-07 22:58:21 -0300

    finalizando projeto clima

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-async document.querySelector('.busca').addEventListener('submit', (event) =>{
+document.querySelector('.busca').addEventListener('submit', async (event) =>{
     event.preventDefault();
 
     let input =document.querySelector('#searchInput').value; // pegando o valor digitado
@@ -8,13 +8,48 @@ async document.querySelector('.busca').addEventListener('submit', (event) =>{
 
             let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=08074f76b5a42159f707bf49687f079c&units=metric`;
 
-            let result = await  fetch(url);
-            let json = await result.jason();
+            let result = await fetch(url);
+            let json = await result.json();
 
-            console.log(json);
+            if (json.cod === 200) {
+                showInfo({
+                    name: json.name,
+                    country: json.sys.country,
+                    temp: json.main.temp,
+                    tempIcon: json.weather[0].icon,
+                    windSpeed: json.wind.speed,
+                    windAngle: json.wind.deg
+                });
+            }else{
+                clearInfor('');
+                showWarning('Não encontramos está localição');
+            }
+
+    } else{
+        clearInfor();
     }
 
 });
+
+function showInfo(json) {
+    showWarning('');
+
+    document.querySelector('.resultado').style.display ='block';
+
+    document.querySelector('.titulo').innerHTML = `${json.name}, ${json.country}`;
+    document.querySelector('.tempInfo').innerHTML = `${json.temp} <sup>°C</sup>`;
+    document.querySelector('.ventoInfo').innerHTML = `${json.windSpeed} <span>km/h</span>`;
+
+    document.querySelector('.temp img').setAttribute('src', `http://openweathermap.org/img/wn/${json.tempIcon}@2x.png`);
+    document.querySelector('.ventoPonto').style.transform = `rotate(${json.windAngle-90}deg)`;
+
+
+}
+
+function clearInfor(){
+    showWarning('');
+    document.querySelector('.resultado').style.display = 'none';
+}
 
 function showWarning(mgs){
     document.querySelector('.aviso').innerHTML = mgs;
